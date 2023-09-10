a2e86abed10f56ea22088b3b5df2a32b77052207
olivvben <dophis96@gmail.com>
2023-09-10 16:24:58 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - prints the alphabet in lowercase,
+ * 		followed by a new liner
+ * Return: always 0
+ */
+
+int main (void)
+{
+	int c;
+	for (c = 97; c <= 122; c++)
+		{
+			putchar(c);
+		}
+	putchar('\n');
+	return (0);
+
