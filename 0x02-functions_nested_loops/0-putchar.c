eb04c48d0b07f72a231c838c2ccb7fe9ef5048af
olivvben <dophis96@gmail.com>
2023-09-12 12:57:46 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-	char data[] = "_putchar\n";
+	char data[] = "_putchar";
 		int i;
 
-		for (i = 0; i  < 10; i++)
+		for (i = 0; i  < 8; i++)
 		{
 			_putchar(data[i]);
 		}
+		_putchar(10);
 	return (0);
 }
 
